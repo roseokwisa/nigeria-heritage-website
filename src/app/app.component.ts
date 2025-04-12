98a04a6ec5ad6fc6be93797d9ec4c23a64278114
rose <okwisarose77@gmail.com>
2025-04-12 23:07:28 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'GhanaHeritageWebsite';
+export class AppComponent implements OnInit{
+  title = 'NigeriaHeritageWebsite';
+  activeTab: string = 'home'; // Default tab
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Check if there is a saved tab in localStorage
+    const savedTab = localStorage.getItem('activeTab');
+    
+    if (savedTab) {
+      this.activeTab = savedTab;  // Load last active tab
+    } else {
+      this.activeTab = 'home'; // Default to Home if no saved tab
+    }}
+
+    setActiveTab(tabName: string): void {
+      this.activeTab = tabName;  // Set active tab
+      localStorage.setItem('activeTab', tabName); // Save tab selection
+    }
 }
