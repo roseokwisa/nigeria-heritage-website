9d7fb9b215c1514e23d4c882781b52720ac6be61
roseokwisa <rozyokwisa@gmail.com>
2025-04-13 09:05:45 +0300

    name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GhanaHeritageWebsite'`, () => {
+  it(`should have as title 'NigeriaeritageWebsite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GhanaHeritageWebsite');
+    expect(app.title).toEqual('NigeriaHeritageWebsite');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('GhanaHeritageWebsite app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('NigeriaHeritageWebsite app is running!');
   });
 });
